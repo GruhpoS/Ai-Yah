4a00628f1e7b82304b4d03d25bdf60194390f662
weeraya <weeraya_fai@hotmail.com>
2011-08-17 11:17:58 +0700

    key

---
+++ File/keyPad/keyPad/Form1.Designer.cs
@@ -0,0 +1,399 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.A = new System.Windows.Forms.Button();
+            this.E = new System.Windows.Forms.Button();
+            this.D = new System.Windows.Forms.Button();
+            this.C = new System.Windows.Forms.Button();
+            this.B = new System.Windows.Forms.Button();
+            this.G = new System.Windows.Forms.Button();
+            this.F = new System.Windows.Forms.Button();
+            this.M = new System.Windows.Forms.Button();
+            this.N = new System.Windows.Forms.Button();
+            this.I = new System.Windows.Forms.Button();
+            this.J = new System.Windows.Forms.Button();
+            this.K = new System.Windows.Forms.Button();
+            this.L = new System.Windows.Forms.Button();
+            this.H = new System.Windows.Forms.Button();
+            this.T = new System.Windows.Forms.Button();
+            this.U = new System.Windows.Forms.Button();
+            this.P = new System.Windows.Forms.Button();
+            this.Q = new System.Windows.Forms.Button();
+            this.R = new System.Windows.Forms.Button();
+            this.S = new System.Windows.Forms.Button();
+            this.O = new System.Windows.Forms.Button();
+            this.W = new System.Windows.Forms.Button();
+            this.X = new System.Windows.Forms.Button();
+            this.Y = new System.Windows.Forms.Button();
+            this.Z = new System.Windows.Forms.Button();
+            this.V = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // A
+            // 
+            this.A.Location = new System.Drawing.Point(127, 334);
+            this.A.Name = "A";
+            this.A.Size = new System.Drawing.Size(41, 33);
+            this.A.TabIndex = 0;
+            this.A.Text = "A";
+            this.A.UseVisualStyleBackColor = true;
+            this.A.Click += new System.EventHandler(this.A_Click);
+            // 
+            // E
+            // 
+            this.E.Location = new System.Drawing.Point(385, 334);
+            this.E.Name = "E";
+            this.E.Size = new System.Drawing.Size(41, 33);
+            this.E.TabIndex = 1;
+            this.E.Text = "E";
+            this.E.UseVisualStyleBackColor = true;
+            this.E.Click += new System.EventHandler(this.E_Click);
+            // 
+            // D
+            // 
+            this.D.Location = new System.Drawing.Point(321, 334);
+            this.D.Name = "D";
+            this.D.Size = new System.Drawing.Size(41, 33);
+            this.D.TabIndex = 2;
+            this.D.Text = "D";
+            this.D.UseVisualStyleBackColor = true;
+            this.D.Click += new System.EventHandler(this.D_Click);
+            // 
+            // C
+            // 
+            this.C.Location = new System.Drawing.Point(255, 334);
+            this.C.Name = "C";
+            this.C.Size = new System.Drawing.Size(41, 33);
+            this.C.TabIndex = 3;
+            this.C.Text = "C";
+            this.C.UseVisualStyleBackColor = true;
+            this.C.Click += new System.EventHandler(this.C_Click);
+            // 
+            // B
+            // 
+            this.B.Location = new System.Drawing.Point(192, 334);
+            this.B.Name = "B";
+            this.B.Size = new System.Drawing.Size(41, 33);
+            this.B.TabIndex = 4;
+            this.B.Text = "B";
+            this.B.UseVisualStyleBackColor = true;
+            this.B.Click += new System.EventHandler(this.B_Click);
+            // 
+            // G
+            // 
+            this.G.Location = new System.Drawing.Point(509, 334);
+            this.G.Name = "G";
+            this.G.Size = new System.Drawing.Size(41, 33);
+            this.G.TabIndex = 5;
+            this.G.Text = "G";
+            this.G.UseVisualStyleBackColor = true;
+            this.G.Click += new System.EventHandler(this.G_Click);
+            // 
+            // F
+            // 
+            this.F.Location = new System.Drawing.Point(447, 334);
+            this.F.Name = "F";
+            this.F.Size = new System.Drawing.Size(41, 33);
+            this.F.TabIndex = 6;
+            this.F.Text = "F";
+            this.F.UseVisualStyleBackColor = true;
+            this.F.Click += new System.EventHandler(this.F_Click);
+            // 
+            // M
+            // 
+            this.M.Location = new System.Drawing.Point(447, 390);
+            this.M.Name = "M";
+            this.M.Size = new System.Drawing.Size(41, 33);
+            this.M.TabIndex = 13;
+            this.M.Text = "M";
+            this.M.UseVisualStyleBackColor = true;
+            this.M.Click += new System.EventHandler(this.M_Click);
+            // 
+            // N
+            // 
+            this.N.Location = new System.Drawing.Point(509, 390);
+            this.N.Name = "N";
+            this.N.Size = new System.Drawing.Size(41, 33);
+            this.N.TabIndex = 12;
+            this.N.Text = "N";
+            this.N.UseVisualStyleBackColor = true;
+            this.N.Click += new System.EventHandler(this.N_Click);
+            // 
+            // I
+            // 
+            this.I.Location = new System.Drawing.Point(192, 390);
+            this.I.Name = "I";
+            this.I.Size = new System.Drawing.Size(41, 33);
+            this.I.TabIndex = 11;
+            this.I.Text = "I";
+            this.I.UseVisualStyleBackColor = true;
+            this.I.Click += new System.EventHandler(this.I_Click);
+            // 
+            // J
+            // 
+            this.J.Location = new System.Drawing.Point(255, 390);
+            this.J.Name = "J";
+            this.J.Size = new System.Drawing.Size(41, 33);
+            this.J.TabIndex = 10;
+            this.J.Text = "J";
+            this.J.UseVisualStyleBackColor = true;
+            this.J.Click += new System.EventHandler(this.J_Click);
+            // 
+            // K
+            // 
+            this.K.Location = new System.Drawing.Point(321, 390);
+            this.K.Name = "K";
+            this.K.Size = new System.Drawing.Size(41, 33);
+            this.K.TabIndex = 9;
+            this.K.Text = "K";
+            this.K.UseVisualStyleBackColor = true;
+            this.K.Click += new System.EventHandler(this.K_Click);
+            // 
+            // L
+            // 
+            this.L.Location = new System.Drawing.Point(385, 390);
+            this.L.Name = "L";
+            this.L.Size = new System.Drawing.Size(41, 33);
+            this.L.TabIndex = 8;
+            this.L.Text = "L";
+            this.L.UseVisualStyleBackColor = true;
+            this.L.Click += new System.EventHandler(this.L_Click);
+            // 
+            // H
+            // 
+            this.H.Location = new System.Drawing.Point(127, 390);
+            this.H.Name = "H";
+            this.H.Size = new System.Drawing.Size(41, 33);
+            this.H.TabIndex = 7;
+            this.H.Text = "H";
+            this.H.UseVisualStyleBackColor = true;
+            this.H.Click += new System.EventHandler(this.H_Click);
+            // 
+            // T
+            // 
+            this.T.Location = new System.Drawing.Point(447, 443);
+            this.T.Name = "T";
+            this.T.Size = new System.Drawing.Size(41, 33);
+            this.T.TabIndex = 27;
+            this.T.Text = "T";
+            this.T.UseVisualStyleBackColor = true;
+            this.T.Click += new System.EventHandler(this.T_Click);
+            // 
+            // U
+            // 
+            this.U.Location = new System.Drawing.Point(509, 443);
+            this.U.Name = "U";
+            this.U.Size = new System.Drawing.Size(41, 33);
+            this.U.TabIndex = 26;
+            this.U.Text = "U";
+            this.U.UseVisualStyleBackColor = true;
+            this.U.Click += new System.EventHandler(this.U_Click);
+            // 
+            // P
+            // 
+            this.P.Location = new System.Drawing.Point(192, 443);
+            this.P.Name = "P";
+            this.P.Size = new System.Drawing.Size(41, 33);
+            this.P.TabIndex = 25;
+            this.P.Text = "P";
+            this.P.UseVisualStyleBackColor = true;
+            this.P.Click += new System.EventHandler(this.P_Click);
+            // 
+            // Q
+            // 
+            this.Q.Location = new System.Drawing.Point(255, 443);
+            this.Q.Name = "Q";
+            this.Q.Size = new System.Drawing.Size(41, 33);
+            this.Q.TabIndex = 24;
+            this.Q.Text = "Q";
+            this.Q.UseVisualStyleBackColor = true;
+            this.Q.Click += new System.EventHandler(this.Q_Click);
+            // 
+            // R
+            // 
+            this.R.Location = new System.Drawing.Point(321, 443);
+            this.R.Name = "R";
+            this.R.Size = new System.Drawing.Size(41, 33);
+            this.R.TabIndex = 23;
+            this.R.Text = "R";
+            this.R.UseVisualStyleBackColor = true;
+            this.R.Click += new System.EventHandler(this.R_Click);
+            // 
+            // S
+            // 
+            this.S.Location = new System.Drawing.Point(385, 443);
+            this.S.Name = "S";
+            this.S.Size = new System.Drawing.Size(41, 33);
+            this.S.TabIndex = 22;
+            this.S.Text = "S";
+            this.S.UseVisualStyleBackColor = true;
+            this.S.Click += new System.EventHandler(this.S_Click);
+            // 
+            // O
+            // 
+            this.O.Location = new System.Drawing.Point(127, 443);
+            this.O.Name = "O";
+            this.O.Size = new System.Drawing.Size(41, 33);
+            this.O.TabIndex = 21;
+            this.O.Text = "O";
+            this.O.UseVisualStyleBackColor = true;
+            this.O.Click += new System.EventHandler(this.O_Click);
+            // 
+            // W
+            // 
+            this.W.Location = new System.Drawing.Point(257, 498);
+            this.W.Name = "W";
+            this.W.Size = new System.Drawing.Size(41, 33);
+            this.W.TabIndex = 32;
+            this.W.Text = "W";
+            this.W.UseVisualStyleBackColor = true;
+            this.W.Click += new System.EventHandler(this.W_Click);
+            // 
+            // X
+            // 
+            this.X.Location = new System.Drawing.Point(320, 498);
+            this.X.Name = "X";
+            this.X.Size = new System.Drawing.Size(41, 33);
+            this.X.TabIndex = 31;
+            this.X.Text = "X";
+            this.X.UseVisualStyleBackColor = true;
+            this.X.Click += new System.EventHandler(this.X_Click);
+            // 
+            // Y
+            // 
+            this.Y.Location = new System.Drawing.Point(386, 498);
+            this.Y.Name = "Y";
+            this.Y.Size = new System.Drawing.Size(41, 33);
+            this.Y.TabIndex = 30;
+            this.Y.Text = "Y";
+            this.Y.UseVisualStyleBackColor = true;
+            this.Y.Click += new System.EventHandler(this.Y_Click);
+            // 
+            // Z
+            // 
+            this.Z.Location = new System.Drawing.Point(450, 498);
+            this.Z.Name = "Z";
+            this.Z.Size = new System.Drawing.Size(41, 33);
+            this.Z.TabIndex = 29;
+            this.Z.Text = "Z";
+            this.Z.UseVisualStyleBackColor = true;
+            this.Z.Click += new System.EventHandler(this.Z_Click);
+            // 
+            // V
+            // 
+            this.V.Location = new System.Drawing.Point(192, 498);
+            this.V.Name = "V";
+            this.V.Size = new System.Drawing.Size(41, 33);
+            this.V.TabIndex = 28;
+            this.V.Text = "V";
+            this.V.UseVisualStyleBackColor = true;
+            this.V.Click += new System.EventHandler(this.V_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(221, 178);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(217, 20);
+            this.textBox1.TabIndex = 33;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(692, 666);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.W);
+            this.Controls.Add(this.X);
+            this.Controls.Add(this.Y);
+            this.Controls.Add(this.Z);
+            this.Controls.Add(this.V);
+            this.Controls.Add(this.T);
+            this.Controls.Add(this.U);
+            this.Controls.Add(this.P);
+            this.Controls.Add(this.Q);
+            this.Controls.Add(this.R);
+            this.Controls.Add(this.S);
+            this.Controls.Add(this.O);
+            this.Controls.Add(this.M);
+            this.Controls.Add(this.N);
+            this.Controls.Add(this.I);
+            this.Controls.Add(this.J);
+            this.Controls.Add(this.K);
+            this.Controls.Add(this.L);
+            this.Controls.Add(this.H);
+            this.Controls.Add(this.F);
+            this.Controls.Add(this.G);
+            this.Controls.Add(this.B);
+            this.Controls.Add(this.C);
+            this.Controls.Add(this.D);
+            this.Controls.Add(this.E);
+            this.Controls.Add(this.A);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button A;
+        private System.Windows.Forms.Button E;
+        private System.Windows.Forms.Button D;
+        private System.Windows.Forms.Button C;
+        private System.Windows.Forms.Button B;
+        private System.Windows.Forms.Button G;
+        private System.Windows.Forms.Button F;
+        private System.Windows.Forms.Button M;
+        private System.Windows.Forms.Button N;
+        private System.Windows.Forms.Button I;
+        private System.Windows.Forms.Button J;
+        private System.Windows.Forms.Button K;
+        private System.Windows.Forms.Button L;
+        private System.Windows.Forms.Button H;
+        private System.Windows.Forms.Button T;
+        private System.Windows.Forms.Button U;
+        private System.Windows.Forms.Button P;
+        private System.Windows.Forms.Button Q;
+        private System.Windows.Forms.Button R;
+        private System.Windows.Forms.Button S;
+        private System.Windows.Forms.Button O;
+        private System.Windows.Forms.Button W;
+        private System.Windows.Forms.Button X;
+        private System.Windows.Forms.Button Y;
+        private System.Windows.Forms.Button Z;
+        private System.Windows.Forms.Button V;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ File/keyPad/keyPad/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void A_Click(object sender, EventArgs e)
+        {
+            string A = "A";
+        }
+
+        private void B_Click(object sender, EventArgs e)
+        {
+            string B = "B";
+        }
+
+        private void C_Click(object sender, EventArgs e)
+        {
+            string C = "C";
+        }
+
+        private void D_Click(object sender, EventArgs e)
+        {
+            string D = "D";
+        }
+
+        private void E_Click(object sender, EventArgs e)
+        {
+            string E = "E";
+        }
+
+        private void F_Click(object sender, EventArgs e)
+        {
+            string F = "F";
+        }
+
+        private void G_Click(object sender, EventArgs e)
+        {
+            string G = "G";
+        }
+
+        private void H_Click(object sender, EventArgs e)
+        {
+            string H = "H";
+        }
+
+        private void I_Click(object sender, EventArgs e)
+        {
+            string I = "I";
+        }
+
+        private void J_Click(object sender, EventArgs e)
+        {
+            string J = "J";
+        }
+
+        private void K_Click(object sender, EventArgs e)
+        {
+            string K = "K";
+        }
+
+        private void L_Click(object sender, EventArgs e)
+        {
+            string L = "L";
+        }
+
+        private void M_Click(object sender, EventArgs e)
+        {
+            string M = "M";
+        }
+
+        private void N_Click(object sender, EventArgs e)
+        {
+            string N = "N";
+        }
+
+        private void O_Click(object sender, EventArgs e)
+        {
+            string O = "O";
+        }
+
+        private void P_Click(object sender, EventArgs e)
+        {
+            string P = "P";
+        }
+
+        private void Q_Click(object sender, EventArgs e)
+        {
+            string Q = "Q";
+        }
+
+        private void R_Click(object sender, EventArgs e)
+        {
+            string R = "R";
+        }
+
+        private void S_Click(object sender, EventArgs e)
+        {
+            string S = "S";
+        }
+
+        private void T_Click(object sender, EventArgs e)
+        {
+            string T = "T";
+        }
+
+        private void U_Click(object sender, EventArgs e)
+        {
+            string U = "U";
+        }
+
+        private void V_Click(object sender, EventArgs e)
+        {
+            string V = "V";
+        }
+
+        private void W_Click(object sender, EventArgs e)
+        {
+            string W = "W";
+        }
+
+        private void X_Click(object sender, EventArgs e)
+        {
+            string X = "X";
+        }
+
+        private void Y_Click(object sender, EventArgs e)
+        {
+            string Y = "Y";
+        }
+
+        private void Z_Click(object sender, EventArgs e)
+        {
+            string Z = "Z";
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
